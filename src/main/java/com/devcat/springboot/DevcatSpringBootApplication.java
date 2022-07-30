bf4de4671c290014ec34e91accdc8a7eeae18281
MSG <milk6645@gmail.com>
2022-07-30 12:35:15 +0900

    Add #2 - controllers
    컨트롤러 추가

---
+++ src/main/java/com/devcat/springboot/DevcatSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.devcat.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevcatSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevcatSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devcat/springboot/DevcatSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devcat.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevcatSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
